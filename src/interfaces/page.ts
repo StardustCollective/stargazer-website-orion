6a351b07282f36d73c31b7ad8f28df78073fa7a4
Cesar Miranda <cesar@Cesars-MacBook-Air.local>
2021-05-20 12:43:09 -0700

    Fixed: Typos

--- src/interfaces/page.ts
+++ src/interfaces/page.ts
@@ -4,18 +4,17 @@ import React from "react";
 // Page Interface
 ////////////////////////
 
-
 /**
  *  @param {Object} Page A page object as defined by the Page interface.
- *  @param {string} Page.route A requried string defining a route that will 
+ *  @param {string} Page.route A required string defining a route that will 
  *  be displayed in the URL
  *  @param {React.FC} Page.surface A required page view component that will 
  *  be displayed when the current url matched the route.
  *  @param {string} Page.title A optional string that will be displayed in 
  *  the sidebar or footer navigation when the page is registered to a 
  *  page list.
- *  @param {React.FC} Page.icon A optional svg icon compoennt that will be displayed
- * in the sidebar when a page os regisred to the side bar list.
+ *  @param {React.FC} Page.icon A optional svg icon component that will be displayed
+ * in the sidebar when a page os registered to the side bar list.
  */
 
 export interface Page {
