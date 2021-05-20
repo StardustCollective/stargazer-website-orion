05018ed26fc4e0dcc8e42ced8d6fc34df1993a13
Cesar Miranda <cesar@Cesars-MacBook-Air.local>
2021-05-20 16:26:35 -0700

    Changed: Restructures the Page interface to have a nested navigation object to store all navigation related properties

--- src/interfaces/page.ts
+++ src/interfaces/page.ts
@@ -1,8 +1,21 @@
 import React from "react";
 
 ////////////////////////
-// Page Interface
+// Interfaces
 ////////////////////////
+
+/**
+ * @typedef {object} Navigation Options applied the the left sidebar navigation component.
+ * @param {React.FC} Navigation.icon A optional svg icon component that will be displayed
+ * in the sidebar when a page is registered to the side bar list.
+ * @param {boolean} Navigation.disabled A optional flag that can disable the page in the sidebar
+ * navigation.
+ */
+
+interface Navigation {
+  icon?: React.FC,
+  disabled?: boolean,
+}
 
 /**
  *  @param {Object} Page A page object as defined by the Page interface.
@@ -13,13 +26,12 @@ import React from "react";
  *  @param {string} Page.title A optional string that will be displayed in 
  *  the sidebar or footer navigation when the page is registered to a 
  *  page list.
- *  @param {React.FC} Page.icon A optional svg icon component that will be displayed
- * in the sidebar when a page os registered to the side bar list.
  */
 
 export interface Page {
-  title: string,
+  title?: string,
   route: string,
   surface: React.FC,
-  icon?: React.FC,
+  navigation?: Navigation,
 };
+
