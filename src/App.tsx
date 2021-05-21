411e07d9a95161d399b136afa1aeeb2bb8f7a8bf
Cesar Miranda <cesar@Cesars-MacBook-Air.local>
2021-05-20 19:53:15 -0700

    Removed: Uneeded PAGES import

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./styles/global.scss";
 import "./styles/index.scss";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { PAGES, LoadPages } from './pages';
+import { LoadPages } from './pages';
 
 function App() {
   return (
