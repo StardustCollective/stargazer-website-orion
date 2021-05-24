f17b8cad4805aff6b94c66c17f2fde089ca51fd6
Cesar Miranda <cmirandac665@gmail.com>
2021-05-24 09:21:46 -0700

    Added: formatString utility file

---
+++ src/util/formatString.ts
@@ -0,0 +1,12 @@
+/**
+ * Formats a credit card number
+ * @param {strong} value A string of credit card numbers 
+ * @returns A formatted string example 5555 5555 5555 5555
+ */
+function creditCard(value: string): string {
+  if(value){
+    return value.replace(/\s+/g, '').match(/.{1,4}/g).join(' ');
+  }
+}
+
+export default { creditCard };
