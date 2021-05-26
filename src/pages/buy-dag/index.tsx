efbffe000be2fba73fab9f7b9a692323cfab656b
Cesar Miranda <cmirandac665@gmail.com>
2021-05-25 21:46:04 -0700

    Refactored: buyDag to use new SteOneBuyDag component

--- src/pages/buy-dag/index.tsx
+++ src/pages/buy-dag/index.tsx
@@ -4,11 +4,14 @@ import { useSelector } from "react-redux";
 import { RootState } from "src/redux/reducers";
 
 import {
-  BuyDagForm,
+  // BuyDagForm,
   BuyDagFormStep1,
   // BuyDagFormStep2,
   TransactionReceipt,
 } from "src/components/feature/buyDag/purchaseForm";
+
+import StepOneGetDag from 'src/components/feature/buyDag/purchaseForm/stepOneGetDag';
+
 
 import styles from "./index.module.scss";
 import ConnectStargazer from "src/components/feature/buyDag/connectStargazer";
@@ -137,7 +140,7 @@ const BuyDag: React.FC = () => {
     switch (step) {
       case 1:
         return (
-          <BuyDagForm
+          <StepOneGetDag
             nextStep={() => {
               setStep(2);
             }}
